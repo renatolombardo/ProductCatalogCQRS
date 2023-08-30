830b76682db86962ca672aa2a02c78187c71120a
Renato Lombardo <E3207@itsector.pt>
2023-08-30 19:10:49 +0100

    Endpoin Extension

---
+++ ProductCatalogCQRS/Extensions/EndpointExtension.cs
@@ -0,0 +1,106 @@
+﻿using MediatR;
+using ProductCatalogCQRS.Commands;
+using ProductCatalogCQRS.Models;
+using ProductCatalogCQRS.Queries;
+
+namespace ProductCatalogCQRS.Extensions;
+
+public static class EndpointExtension
+{
+    public static WebApplication ConfigureEndpoints(this WebApplication app)
+    {
+        app.MapGet("/product", async (IMediator _mediator) =>
+        {
+            try
+            {
+                var command = new GetAllProductsQuery();
+                var response = await _mediator.Send(command);
+                return response is not null ? Results.Ok(response) : Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+
+        app.MapGet("/product/{:id}", async (IMediator _mediator, int id) =>
+        {
+            try
+            {
+                var command = new GetProductbyIdQuery { Id = id };
+
+                var response = await _mediator.Send(command);
+
+                return response is not null ? Results.Ok(response) : Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+
+        app.MapPost("/product", async (IMediator _mediator, Product product) =>
+        {
+            try
+            {
+                var command = new CreateProductCommand
+                {
+                    Category = product.Category,
+                    Description = product.Description,
+                    Name = product.Name,
+                    Price = product.Price
+                };
+
+                var response = _mediator.Send(command);
+
+                return response is not null ? Results.Ok(response) : Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+
+        app.MapPut("/product", async (IMediator _mediator, Product product) =>
+        {
+            try
+            {
+                var command = new UpdateProductCommand
+                {
+                    Active = product.Active,
+                    Category = product.Category,
+                    Description = product.Description,
+                    Name = product.Name,
+                    Price = product.Price,
+                    Id = product.Id
+                };
+
+                var response = await _mediator.Send(command);
+
+                return response is not null ? Results.Ok(response) : Results.NotFound(product.Description);
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+
+        app.MapDelete("/product", async (IMediator _mediator, int id) =>
+        {
+            try
+            {
+                var command = new DeleteProductCommand { Id = id };
+
+                var response = await _mediator.Send(command);
+
+                return response is not null ? Results.Ok(response) : Results.NotFound();
+            }
+            catch (Exception ex)
+            {
+                return Results.BadRequest(ex.Message);
+            }
+        });
+
+        return app;
+    }
+}
--- ProductCatalogCQRS/Program.cs
+++ ProductCatalogCQRS/Program.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using ProductCatalogCQRS.Commands;
 using ProductCatalogCQRS.Data;
+using ProductCatalogCQRS.Extensions;
 using ProductCatalogCQRS.Models;
 using ProductCatalogCQRS.Queries;
 using System.Reflection;
@@ -27,97 +28,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/product", async (IMediator _mediator) =>
-{
-	try
-	{
-		var command = new GetAllProductsQuery();
-		var response = await _mediator.Send(command);
-		return response is not null ? Results.Ok(response) : Results.NotFound();
-	}
-	catch (Exception ex)
-	{
-		return Results.BadRequest(ex.Message);
-	}
-});
-
-app.MapGet("/product/{:id}", async(IMediator _mediator, int id) =>
-{
-    try
-    {
-        var command = new GetProductbyIdQuery { Id = id };
-
-        var response = await _mediator.Send(command);
-
-        return response is not null ? Results.Ok(response) : Results.NotFound();
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-});
-
-app.MapPost("/product", async (IMediator _mediator, Product product) =>
-{
-    try
-    {
-        var command = new CreateProductCommand
-        {
-            Category = product.Category,
-            Description = product.Description,
-            Name = product.Name,
-            Price = product.Price
-        };
-
-        var response = _mediator.Send(command);
-
-        return response is not null ? Results.Ok(response) : Results.NotFound();
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-});
-
-app.MapPut("/product", async(IMediator _mediator, Product product) =>
-{
-    try
-    {
-        var command = new UpdateProductCommand
-        {
-            Active = product.Active,
-            Category = product.Category,
-            Description = product.Description,
-            Name = product.Name,
-            Price = product.Price,
-            Id = product.Id
-        };
-
-        var response = await _mediator.Send(command);
-
-        return response is not null ? Results.Ok(response) : Results.NotFound(product.Description);
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-});
-
-app.MapDelete("/product", async (IMediator _mediator, int id) =>
-{
-    try
-    {
-        var command = new DeleteProductCommand { Id = id };
-
-        var response = await _mediator.Send(command);
-
-        return response is not null ? Results.Ok(response) : Results.NotFound();
-    }
-    catch (Exception ex)
-    {
-        return Results.BadRequest(ex.Message);
-    }
-});
+app.ConfigureEndpoints();
 
 app.Run();
 
